jspricke/python-remind

commit 2c41e7256074aa61d06bb80387abb979993750bb

Simplify timezone handling
See:
https://github.com/jspricke/remind-caldav/issues/15
https://stackoverflow.com/questions/21128695/django-ical-with-vobject-issue-with-pytz

remind.py
@@ -28,7 +28,7 @@ from time import time
 from typing import Any, Iterable, Optional, Union
 from zoneinfo import ZoneInfo
 
-from dateutil import rrule
+from dateutil import rrule, tz
 from vobject import iCalendar
 from vobject.base import Component, readOne
 
@@ -52,7 +52,7 @@ class Remind:
         month -- how many month to parse, will be passed to remind -s
         """
         self._filename = filename if filename else expanduser("~/.reminders")
-        self._localtz = localtz if localtz else ZoneInfo("localtime")
+        self._localtz = localtz if localtz else tz.gettz()
         self._startdate = startdate if startdate else date.today() - timedelta(weeks=12)
         self._month = month
         self._alarm = alarm if alarm else timedelta(minutes=-10)
@@ -117,7 +117,7 @@ class Remind:
                 if "eventstart" in entry:
                     dtstart: Union[datetime, date] = datetime.strptime(
                         entry["eventstart"], "%Y-%m-%dT%H:%M"
-                    ).replace(tzinfo=self._localtz)
+                    ).astimezone(self._localtz)
                 else:
                     dtstart = datetime.strptime(entry["date"], "%Y-%m-%d").date()
 

commit 27bf4887a1db58a49eb3c7572198f36bcec5aa84

Use replace(tzinfo) to adopt the remind timezone
The _localtz option should serve as an override, to add the timezone of
the Remind event, so replace() is correct. astimezone() instead would
shift the event date from the system timezone to _localtz.

Fixes a regression in 2c41e725.

remind.py
@@ -131,7 +131,7 @@ class Remind:
                 if "eventstart" in entry:
                     dtstart: datetime | date = datetime.strptime(
                         entry["eventstart"], "%Y-%m-%dT%H:%M"
-                    ).astimezone(self._localtz)
+                    ).replace(tzinfo=self._localtz)
                 else:
                     dtstart = datetime.strptime(entry["date"], "%Y-%m-%d").date()
 