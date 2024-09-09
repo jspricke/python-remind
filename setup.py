ff7378c24f3c4d230f5f1b5d351f57e80d1cff42
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2024-09-09 10:01:35 +0200

    Version 0.19.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="remind",
-    version="0.19.0",
+    version="0.19.1",
     description="Remind Python library",
     long_description=open("README.rst").read(),
     author="Jochen Sprickerhof",
